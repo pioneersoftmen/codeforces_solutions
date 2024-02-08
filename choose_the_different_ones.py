1021cb7fe32dd7b4c5dd502f578e0c92994799ec
pioneersoftmen <smen78270@gmail.com>
2024-02-08 22:07:31 +0500

    Solution to choose the different ones

--- choose_the_different_ones.py
+++ choose_the_different_ones.py
@@ -1,56 +1,25 @@
-t = int(input())
-
-for _ in range(t):
+def solve():
     n, m, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    if n < k //2 or m < k // 2:
-        print("NO")
-        continue
-    seta = set()
-    setb = set()
-    that = None
-    for i in range(min(m, n)):
-        if 1 <= a[i] <= k:
-            seta.add(a[i])
-        if 1 <= b[i] <= k:
-            setb.add(b[i])
-        if len(seta) + len(setb) >= k and len(seta) >= k // 2 and len(setb) >= k // 2:
-            if len(seta | setb) >= k:
-                print("YES")
-                break
-        that = i
-    else:
-        if len(seta) < k // 2 and len(setb) < k // 2:
-            print("NO")
-        else:
-            if m != n:
-                if m > n:
-                    if len(seta) < k // 2:
-                        print("NO")
-                    else:
-                        while that < m:
-                            if 1 <= b[that] <= k:
-                                setb.add(b[that])
-                            if len(setb) >= k // 2:
-                                if len(seta | setb) >= k and len(seta) >= k // 2 and len(setb) >= k // 2:
-                                    print("YES")
-                            that += 1
-                        else:
-                            print("NO")        
-                else:
-                    if len(setb) < k // 2:
-                        print("NO")
-                    else:
-                        while that <= n:
-                            if 1 <= a[that] <= k:
-                                setb.add(a[that])
-                            if len(setb) >= k // 2:
-                                if len(seta | setb) >= k and len(seta) >= k // 2 and len(setb) >= k // 2:
-                                    print("YES")
-                                    break
-                            that += 1
-                        else:
-                            print("NO")   
-                
+    a = [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+
+    cnt = [0] * (k + 1)
+    for e in a:
+        if e <= k:
+            cnt[e] |= 1
     
+    for e in b:
+        if e <= k:
+            cnt[e] |= 2
+
+    c = [0] * 4
+    for e in cnt:
+        c[e] += 1
+
+    if c[1] > k // 2 or c[2] > k // 2 or c[1] + c[2] + c[3] != k:
+        print("NO")
+    else:
+        print("YES")
+
+for _ in range(int(input())):
+    solve()
--- vanya_and_lanterns.py
+++ vanya_and_lanterns.py
@@ -6,4 +6,4 @@ for i in range(1, n):
     maximum = max(maximum, a[i] - a[i - 1])
 first = a[0] - 0
 second = l - a[-1]
-print(max(maximum / 2, first, second))
+print(max(maximum / 2, first, second))f
